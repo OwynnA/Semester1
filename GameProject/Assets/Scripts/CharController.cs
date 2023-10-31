4c26a0a065e3b0c401ca4088fb9579677b85bf0c
Owynn Armstrong <10932343@uvu.edu>
2023-10-30 23:33:57 -0600

    Got houdini plugin and animation state machine working in unity

---
+++ GameProject/Assets/Scripts/CharController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharController : MonoBehaviour
+{
+    //I got some of this code from the character controller page in the canvas class
+    //the rest came from a game I made last year
+    public float moveSpeed;
+    public float trueSpeed = 5f;
+    public float trueJumpForce = 4f;
+    public float jumpForce;
+    public float gravity = -20f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    Animator animator;
+    public TextMeshProUGUI gameOver;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        jumpForce = trueJumpForce;
+        moveSpeed = trueSpeed;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+
+        // Horizontal movement
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = new Vector3(0, 0, -moveInput);
+        Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed;
+        if(moveInput != 0)
+        {
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+
+        // Apply gravity
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            animator.SetBool("Run", false);
+        }
+        else
+        {
+            velocity.y = 0;
+            animator.SetTrigger("Jump End");
+        }
+
+        // Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+            animator.SetTrigger("Jump");
+        }
+
+        // Apply movement and gravity
+        controller.Move((move + velocity) * Time.deltaTime);
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("Death");
+        gameOver.gameObject.SetActive(true);
+        gameOver.text = "Game Over";
+        Debug.Log("If nothing happened something's wrong");
+    }
+}
---
+++ GameProject/Assets/maya/maya.ma
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b25dc299fa780b0e7ba471cd83a49e3a757b71e8bd07f00c15675c5c69641c18
+size 542212
